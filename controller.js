59ed2d475505b95dabfae9b71ac53d8766144b73
Axel <axelharyanto@outlook.com>
2020-04-21 23:06:30 +0700

    berhasil menampilkan data sparepart dan montir secara keseluruhan

--- controller.js
+++ controller.js
@@ -6,3 +6,25 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok("Aplikasi REST API berjalan !", res)
 };
+
+//menampilkan semua data sparepart
+exports.tampildatasparepart = function (req, res) {
+    connection.query('SELECT * FROM t_sparepart', function (error, rows, fields) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
+};
+
+//menampilkan semua data montir
+exports.tampildatamontir = function (req, res) {
+    connection.query('SELECT * FROM t_montir', function (error, rows, fields) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
+};
