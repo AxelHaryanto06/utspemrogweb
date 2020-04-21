baee2961a8c26967d9e42b618c32db770a531fd0
Axel <axelharyanto@outlook.com>
2020-04-22 00:14:46 +0700

    berhasil menampilkan data sparepart dan montir berdasarkan id

--- controller.js
+++ controller.js
@@ -18,6 +18,19 @@ exports.tampildatasparepart = function (req, res) {
     });
 };
 
+//menampilkan data sparepart berdasarkan id
+exports.tampilsparepartid = function (req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM t_sparepart WHERE id_sparepart = ?', (id),
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res);
+            }
+        });
+};
+
 //menampilkan semua data montir
 exports.tampildatamontir = function (req, res) {
     connection.query('SELECT * FROM t_montir', function (error, rows, fields) {
@@ -27,4 +40,17 @@ exports.tampildatamontir = function (req, res) {
             response.ok(rows, res)
         }
     });
+};
+
+//menampilkan data montir berdasarkan id
+exports.tampilmontirid = function (req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM t_montir WHERE id_montir = ?', (id),
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res);
+            }
+        });
 };
--- routes.js
+++ routes.js
@@ -10,4 +10,8 @@ module.exports = function (app) {
         .get(jsonku.tampildatasparepart);
     app.route('/tampilmontir')
         .get(jsonku.tampildatamontir);
+    app.route('/tampilsparepart/:id')
+        .get(jsonku.tampilsparepartid);
+    app.route('/tampilmontir/:id')
+        .get(jsonku.tampilmontirid);
 }
