261b00bc6e12245766c8f2b2c230f680f5cf3d8f
Axel <axelharyanto@outlook.com>
2020-04-29 22:28:05 +0700

    regsiter user

---
+++ middleware/auth.js
@@ -0,0 +1,43 @@
+var connection = require('../koneksi');
+var mysql = require('mysql');
+var md5 = require('MD5');
+var response = require('../res');
+var jwt = require('jsonwebtoken');
+var config = require('../config/secret');
+var ip = require('ip');
+
+//controller untuk register
+exports.registrasi = function(req, res) {
+    //array post
+    var post = {        
+        email: req.body.email,
+        password: md5(req.body.password),
+        role: req.body.role
+    }
+
+    var query = "SELECT email FROM ?? WHERE ??=?";
+    var table = ["user", "email", post.email]
+
+    query = mysql.format(query,table);
+
+    connection.query(query, function(error, rows) {
+        if (error) {
+            console.log(error);
+        } else {
+            if (rows.length == 0) {
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["user"];
+                query = mysql.format(query, table);
+                connection.query(query, post, function(error, rows) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        response.ok("Berhasil menambahkan data user baru", res);
+                    }
+                });
+            }else {
+                response.ok("Email sudah terdaftar !",res);
+            }
+        }
+    })
+}
